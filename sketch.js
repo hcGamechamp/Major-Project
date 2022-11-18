4ff141030e52348ab5d45e641d53996ea9103cc8
Marquis1302272 <1302272@sps-student.sk.ca>
2022-11-18 11:50:36 -0600

    cocaine and red wine

--- sketch.js
+++ sketch.js
@@ -1,15 +1,34 @@
-// Project Title
-// Your Name
-// Date
+// Angry Birds/ Major Project
+// Josh and Muhammad
+// Nov 18, 2022
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// class  {
+//   constructor() {
+//     this.x = ;
+//     this.y = ;
+//   }
+// }
+
+let state = "start"; 
+let startScreen;
+
+function preload() {
+  startScreen = loadImage("angry birds.jpg");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  if (state === "start") {
+    image(startScreen, windowWidth, windowHeight);
+  }
+}
+
+function systemCall() {
+  
 }
