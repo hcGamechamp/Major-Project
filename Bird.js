16ab9018f5a1684bd419cb886c62ff3fad2ada8f
Marquis1302272 <1302272@sps-student.sk.ca>
2022-12-08 10:31:07 -0600

    deleted the bird class to try it somewhere else first (forgot to push yesterday

--- Bird.js
+++ Bird.js
@@ -1,25 +1,38 @@
-class bird {
-  constructor(x, y, radius) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
+let sprite;
 
-  }
+function setup() {
+  new canvas(242, 100);
 
-  move() {
-    this.x += this.dx;
-    this.y += this.dy;
-    // gravity // if x > (sling shot x number)
-    //world.gravity.y = 10;
-  }
-
-  objectCollision() {
-    
-  }
-
-
-  mapCollision() {
-    // if y <= (idk) bird stops falling and can roll based on the momentem that it had
-    // if x <= (idk) stops the bird from traveling outside the map when hiting the wall
-  }
+  sprite = new sprite();
+  sprite.diameter = 50;
 }
+
+// class bird {
+//   constructor(x, y, radius) {
+//     this.x = x;
+//     this.y = y;
+//     this.radius = radius;
+
+//   }
+
+//   move() {
+//     this.x += this.dx;
+//     this.y += this.dy;
+//     // gravity // if x > (sling shot x number)
+//     //world.gravity.y = 10;
+//   }
+
+//   display() {
+//     circle(this.x, this.y, this.radius*2);
+//   }
+
+//   objectCollision() {
+    
+//   }
+
+
+//   mapCollision() {
+//     // if y <= (windowheight) bird stops falling and can roll based on the momentem that it had
+//     // if x <= (windowwidth) stops the bird from traveling outside the map when hiting the wall
+//   }
+// }
